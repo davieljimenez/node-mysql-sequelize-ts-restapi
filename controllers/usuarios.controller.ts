a62174e2cc9c4e80e486cd90f7ca9b1dd383adc7
Daviel Jimenez <Daviel050399@gmail.com>
2021-03-20 04:41:44 -0400

    Add Put user controller

--- controllers/usuarios.controller.ts
+++ controllers/usuarios.controller.ts
@@ -61,24 +61,35 @@ export const postUsuario = async (req:Request, res:Response) =>{
 }
 
 
-export const putUsuario = (req:Request, res:Response) =>{
+export const putUsuario = async (req:Request, res:Response) =>{
 
     const {id} = req.params;
     const {body} = req;
 
-    res.json({
-        msg:"put usuario", 
-        body,
-        id
-    })
+    try {
+        const usuario = await Usuario.findByPk(id);
+        if(!usuario){
+            return res.status(404).json({
+                msg: `No existe un usuario con el id${id}`
+            });
+        }
+
+        await usuario.update(body);
+
+        res.json(usuario);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Hable con el administadror"
+        })
+        
+    }
 }
 
 export const deleteUsuario = (req:Request, res:Response) =>{
 
     const {id} = req.params;
 
-    res.json({
-        msg:"put usuario",
-        id
-    })
+
 }
--- dist/controllers/usuarios.controller.js
+++ dist/controllers/usuarios.controller.js
@@ -57,22 +57,29 @@ const postUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* ()
     }
 });
 exports.postUsuario = postUsuario;
-const putUsuario = (req, res) => {
+const putUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const { body } = req;
-    res.json({
-        msg: "put usuario",
-        body,
-        id
-    });
-};
+    try {
+        const usuario = yield usuario_model_1.default.findByPk(id);
+        if (!usuario) {
+            return res.status(404).json({
+                msg: `No existe un usuario con el id${id}`
+            });
+        }
+        yield usuario.update(body);
+        res.json(usuario);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Hable con el administadror"
+        });
+    }
+});
 exports.putUsuario = putUsuario;
 const deleteUsuario = (req, res) => {
     const { id } = req.params;
-    res.json({
-        msg: "put usuario",
-        id
-    });
 };
 exports.deleteUsuario = deleteUsuario;
 //# sourceMappingURL=usuarios.controller.js.map
